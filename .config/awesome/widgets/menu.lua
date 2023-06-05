ae624dd2dd34b300ef4f577fff4fb89a68395fd9
Derry Leng <derry.leng@gmail.com>
2023-06-05 17:05:05 +0100

    Fix awesome menu error on first use

--- .config/awesome/widgets/menu.lua
+++ .config/awesome/widgets/menu.lua
@@ -6,8 +6,6 @@ local beautiful = require'beautiful'
 local hotkeys_popup = require'awful.hotkeys_popup'
 require'awful.hotkeys_popup.keys'
 
-local archmenu = require'archmenu'
-
 _M.awesomemenu = {
     {'hotkeys', function() awful.hotkeys_popup.show_help(nil, awful.screen.focused()) end},
     {'manual', vars.terminal .. ' -e man awesome'},
@@ -16,12 +14,24 @@ _M.awesomemenu = {
     {'quit', function() awesome.quit() end},
 }
 
-table.insert(xdgmenu, {'Settings', _M.awesomemenu, beautiful.awesome_icon})
-table.insert(xdgmenu, {'Terminal', vars.terminal})
+if pcall(require, 'archmenu') then
 
-_M.mainmenu = awful.menu{
-    items = xdgmenu
-}
+    local archmenu = require'archmenu'
+
+    table.insert(xdgmenu, {'Settings', _M.awesomemenu, beautiful.awesome_icon})
+    table.insert(xdgmenu, {'Terminal', vars.terminal})
+
+    _M.mainmenu = awful.menu{
+        items = xdgmenu
+    }
+
+else
+
+    _M.mainmenu = awful.menu{
+        items = _M.awesomemenu
+    }
+
+end
 
 _M.launcher = awful.widget.launcher{
     image = beautiful.awesome_icon,
